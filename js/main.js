79fbc6feacad6e74dc91f26e3eacfdd97e7c2e99
Vladimir859 <nuykinvladimir859@gmail.com>
2025-04-01 19:41:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+(function ($) {
+    "use strict";
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#nav').addClass('nav-sticky');
+        } else {
+            $('#nav').removeClass('nav-sticky');
+        }
+    });
+    if ($(window).scrollTop() > 100) {
+        $('#nav').addClass('nav-sticky');
+    }
+    
+    
+    // Smooth scrolling on the navbar links
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 50
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+  
+})(jQuery);
+
+
+function openModal1() {
+    document.getElementById("myModal1").classList.add("show");
+
+}
+
+function closeModal1(event) {
+    if (event.target.classList.contains("modal") || event.target.classList.contains("close-btn")) {
+        document.getElementById("myModal1").classList.remove("show");
+    }
+}
+
